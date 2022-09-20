2abf41bae8a012361c9a63c6311731012a78aea6
selin <selinguneylg@gmail.com>
2022-09-20 23:35:22 +0300

    day 4

---
+++ Day4Homework4/MyDictionary.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<T,A>
+    {
+        T[] keys;
+        A[] values;
+        T[] tempKeys;
+        A[] tempValues;
+
+        public MyDictionary()
+        {
+            keys = new T[0];
+            values = new A[0];
+        }
+
+        public void Add(T key, A value)
+        {
+            tempKeys = keys;
+            keys = new T[keys.Length + 1];
+            tempValues = values;
+            values = new A[values.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+            }
+            
+            keys[keys.Length - 1] = key;
+
+            for (int j = 0; j < tempValues.Length; j++)
+            {
+                values[j] = tempValues[j];
+            }
+
+            values[values.Length - 1] = value;
+        }
+
+        public void List()
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine("türkçe: " + keys[i] + ", ingilizce: " + values[i]);
+            }
+        }
+    }
+}
---
+++ Day4Homework4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> kelimeler = new MyDictionary<string, string>();
+            kelimeler.Add("kalem", "pencil");
+            kelimeler.Add("kitap","book");
+            kelimeler.Add("elma", "apple");
+            kelimeler.Add("türkçe", "turkish");
+            kelimeler.Add("harita", "map");
+            kelimeler.List();
+
+        }
+    }
+}
