2bd09e5b4a99da4c1bb642b3646b77a8e2f9f255
selin <selinguneylg@gmail.com>
2022-09-27 00:30:59 +0300

    edit

--- Day5Homework5/Program.cs
+++ Day5Homework5/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Day5Homework5.Concrete;
+using Day5Homework5.Entities;
+using System;
 
 namespace Day5Homework5
 {
@@ -6,7 +8,18 @@ namespace Day5Homework5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            GamerManager gamerManager = new GamerManager(new GamerCheckManager());
+            gamerManager.Add(new Gamer
+            {
+                Id = 1,
+                YearOfBirth = "1998",
+                FirstName = "Selin",
+                LastName = "Güney",
+                NationalId = 123456789
+            });
+
+            CampaignManager campaignManager = new CampaignManager();
+            campaignManager.Add(new Campaign { Id = 1, Name = "New Season Sales"});
         }
     }
 }
